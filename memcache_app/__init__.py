2546e43c44107a73a5a62f91851634bdcd1355d3
BrianQu <qujianning0401@163.com>
2022-12-14 20:20:37 -0500

    fix host

--- memcache_app/__init__.py
+++ memcache_app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from memcache_app import memcache
 from memcache_app import config
+from memcache_app import memcache_routes
 webapp = Flask(__name__)
 
 ''' Default Type is LRU and default size is 10 MB'''
-config.memcache_obj = memcache.LRUMemCache(10)
-
+config.emcache_obj = memcache.LRUMemCache(10)
+memcache_routes.startup_app()

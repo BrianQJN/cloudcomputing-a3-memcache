3a9060c2ac5338577ad1d92a2b279f8f15e439fb
BrianQu <qujianning0401@163.com>
2022-12-15 17:53:24 -0500

    update report log

--- memcache_app/memcache.py
+++ memcache_app/memcache.py
@@ -22,7 +22,7 @@ class LRUMemCache(LRUCache):
     
     def pushitem(self, key, value):
         response = self.__setitem__(key, value)
-        self.request_count += 1
+        # self.request_count += 1
         return response
 
     def updateitem(self, key, new_value):
@@ -115,7 +115,7 @@ class RRMemCache(RRCache):
     
     def pushitem(self, key, value):
         response = self.__setitem__(key, value)
-        self.request_count += 1
+        # self.request_count += 1
         return response
 
     def updateitem(self, key, new_value):

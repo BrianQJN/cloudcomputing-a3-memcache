af0c0b369a7a340eadda03a4716b71142a55bbf0
BrianQu <qujianning0401@163.com>
2022-12-14 22:00:26 -0500

    update

--- backend_app/__init__.py
+++ backend_app/__init__.py
@@ -3,7 +3,7 @@ import threading
 webapp = Flask(__name__)
 
 from backend_app import main
-from AWS_Log_operator import thread_stats
+from backend_app.AWS_Log_operator import thread_stats
 
 th = threading.Thread(target=thread_stats)
 th.start()
